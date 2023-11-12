f3f8c36ededbaada797643fc17188d067f4f6092
BotIzer <izerboti@gmail.com>
2023-11-12 11:01:01 +0100

    Styled friendlist with placeholder data

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,22 +8,22 @@ function Navigation() {
     color: "yellow",
   }
   return (
-    <Navbar expand='lg' className='bg-body-tertiary' bg='dark' data-bs-theme='dark'>
-      <Container>
-        <Navbar.Brand style={yellowTextStyle} href='#home'>React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto'>
-            <Nav.Link style={yellowTextStyle} href='#home'>Friends</Nav.Link>
-            <Nav.Link style={yellowTextStyle} href='#link'>Search</Nav.Link>
-            <Nav.Link style={yellowTextStyle} href='#link'>User</Nav.Link>
-            <Nav.Link style={yellowTextStyle} href='#link'>Notifications</Nav.Link>
-            <Nav.Link style={yellowTextStyle} href='#link'>Blitz</Nav.Link>
-            <Nav.Link style={yellowTextStyle} href='#home'>Home</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar expand='lg' className='bg-body-tertiary' bg='dark' data-bs-theme='dark'>
+        <Container>
+          <Navbar.Brand style={yellowTextStyle} href='#home'>React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link style={yellowTextStyle} href='#home'>Friends</Nav.Link>
+              <Nav.Link style={yellowTextStyle} href='#link'>Search</Nav.Link>
+              <Nav.Link style={yellowTextStyle} href='#link'>User</Nav.Link>
+              <Nav.Link style={yellowTextStyle} href='#link'>Notifications</Nav.Link>
+              <Nav.Link style={yellowTextStyle} href='#link'>Blitz</Nav.Link>
+              <Nav.Link style={yellowTextStyle} href='#home'>Home</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
   );
 }
 
