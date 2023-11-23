f0861a9441e3e9fe97c39cbb991c72cd7644fc99
BotIzer <izerboti@gmail.com>
2023-11-23 12:31:09 +0100

    seperated home page into own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
 import { useState } from 'react'
 import './styles/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css' 
-import Navigation from './components/Navigation'
-import FriendList from './components/FriendList'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Home from './components/Home'
 
 
 function App() {
 
   return (
     <>
-    <Navigation/>
-    <Container fluid>
-      <Row>
-        <Col className='border' xs={4} sm={3} md={2} lg={2} xl={2} xxl={2}>
-          <FriendList/>
-        </Col>
-        <Col className='d-flex justify-content-center border'>
-          Second Column
-        </Col>
-        <Col className='border' xs={4} sm={3} md={2} lg={2} xl={2} xxl={2}>
-          Third Column
-        </Col>
-      </Row>
-    </Container>
-    
+    <Home></Home>
     </>
   )
 }
