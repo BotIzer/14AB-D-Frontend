86210d0fc341bb6dada478f36859f6252c29f004
markneu22 <markneu22@gmail.com>
2023-11-26 09:45:03 +0100

    Benji help

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import axios from '../api/axios'
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function Register(){
 
@@ -63,16 +63,30 @@ const handleSubmit = async (e)=>{
 
   return (
     <>
-      
+      <section>
+        <p ref={errRef} className={errMsg ? "errmsg":"offscreen"} aria-live="assertive">{errMsg}</p>
+      </section>
       <Navigation></Navigation>
       <Container >
         <Row className="justify-content-center" xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
           <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto" className="justify-content-center border border-warning rounded mt-5" style={{backgroundColor: "#4a4b4f"}}>
             <div className="border border-warning rounded px-5 py-2 my-3" style={{height: "400px"}}>
               <Form>
-              <Form.Group className="mb-3" controlId="formGroupEmail">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control type="text" placeholder="Enter email" />
+              <Form.Group className="mb-3" controlId="username">
+                  <Form.Label>Username: 
+                    {/* TODO: Fix this thing */}
+                    <span class="hide" className=".d-none" color="green">+</span>
+                    <span className={validName || !user ? "hide" : "invalid"} color="red">-</span>
+                  </Form.Label>
+                  <Form.Control type="text" placeholder="Enter username"
+                   ref={userRef} autoComplete="off" onChange={(e)=>setUser(e.target.value)}
+                   required aria-invalid={validName?"false":"true"} aria-describedby="uidnote" 
+                   nFocus={()=>setUserFocus(true)} onBlur={()=> setUserFocus(false)}/>
+                   <p id="uidnote" className={userFocus && user && !validName ? "instructions": "offscreen"}>
+                    4 to 24 characters. <br/>
+                    Must begin with a letter <br/>
+                    Letters, numbers, underscores, hyphens are allowed.
+                   </p>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formGroupEmail">
                   <Form.Label>Email address</Form.Label>
