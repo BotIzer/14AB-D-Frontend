715af09336c147d7078a75d62211d6313ec1b891
markneu22 <markneu22@gmail.com>
2023-11-26 09:56:49 +0100

    Fixed Password problem

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -115,9 +115,11 @@ const handleSubmit = async (e)=>{
                   <Form.Label>Email address</Form.Label>
                   <Form.Control type="email" placeholder="Enter email" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formGroupPassword">
+                <Form.Group className="mb-3" controlId="password">
                   <Form.Label>Password</Form.Label>
                   <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="password_again">
                   <Form.Label>Confirm Password</Form.Label>
                   <Form.Control type="password" placeholder="Password Again" />
                 </Form.Group>
