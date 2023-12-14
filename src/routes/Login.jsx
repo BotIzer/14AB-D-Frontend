1a36ceec5a1d69494088e58e59e3c6c6e67a31dd
Lajtaib0801 <lajtaibenja@gmail.com>
2023-12-14 23:08:17 +0100

    404 error message added

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -55,12 +55,13 @@ export default function Login(){
         }
       )
       navigate('/');
+
     } catch (err) {
       if (!err?.response) {
         setErrMsg('No Server Response')
       }
-      else if(err.response?.status === 400){
-        setErrMsg("Missing Username or Password");
+      else if(err.response?.status === 404){
+        setErrMsg(err.response.data.message);
       }
       else if (err.response?.status === 401) {
         setErrMsg("Unauthorized")
