b11ab194b6d836e0af13e8d330b090755bda9f4d
markneu22 <markneu22@gmail.com>
2024-01-22 17:18:13 +0100

    Fixed merge problems (trying Github Desktop rn)

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,5 @@
 import Navigation from '../components/Navigation'
 import FriendList from '../components/FriendList'
-<<<<<<< HEAD
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -39,29 +38,3 @@ export default function Home() {
   );
 
 }
-=======
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-
-export default function Home() {
-  return (
-    <>
-      <Navigation />
-      <Container fluid>
-        <Row>
-          <Col className="border" xs={4} sm={3} md={2} lg={2} xl={2} xxl={2}>
-            <FriendList />
-          </Col>
-          <Col className="d-flex justify-content-center border">
-            Second Column
-          </Col>
-          <Col className="border" xs={4} sm={3} md={2} lg={2} xl={2} xxl={2}>
-            Third Column
-          </Col>
-        </Row>
-      </Container>
-    </>
-  )
-}
->>>>>>> main
