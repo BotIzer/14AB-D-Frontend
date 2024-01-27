15074320ed9c137020be3c7e53df336a50d8aee6
BotIzer <izerboti@gmail.com>
2024-01-27 12:15:16 +0100

    Added quill package, text editor styling

---
+++ src/components/TextEditor.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+function TextEditor() {
+  const [value, setValue] = useState('');
+
+  return <ReactQuill style={{color: "white"}} className='text-editor' theme="snow" x value={value} onChange={setValue} />;
+}
+
+export default TextEditor;
--- src/routes/CreatePost.jsx
+++ src/routes/CreatePost.jsx
@@ -2,26 +2,32 @@ import { FormGroup, FormLabel } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
+import TextEditor from "../components/TextEditor";
+import Navigation from "../components/Navigation";
 
 function CreatePost() {
   return (
-    <Tabs
-      defaultActiveKey="post"
-      className="d-flex justify-content-center mb-5 mx-auto my-5 border"
-      style={{ width: "40vw" }}
-      justify
-    >
-      <Tab eventKey="post" title="Post">
-        <FormGroup className="w-25 mx-auto">
-          <FormLabel></FormLabel>
+    <>
+      <Navigation></Navigation>
+      <Tabs
+        defaultActiveKey="post"
+        className="d-flex mb-5 mx-auto my-5"
+        style={{ width: "40vw"}}
+        justify
+      >
+        <Tab eventKey="post" title="Post" className="d-flex justify-content-center border w-50 mx-auto my-5">
+          <FormGroup className="p-2 w-100 h-100">
+            <Form.Label className="secondary">Title</Form.Label>
+            <Form.Control size="lg" type="text" placeholder="Title" className="mb-3" />
+            <Form.Label className="secondary">Body</Form.Label>
+            <TextEditor className='h-100'></TextEditor>
+          </FormGroup>
+        </Tab>
+        <Tab eventKey="file" title="Add File">
           <Form.Control size="lg" type="text" placeholder="Large text" />
-          <Form.Control as="textarea"></Form.Control>
-        </FormGroup>
-      </Tab>
-      <Tab eventKey="file" title="Add File">
-        <Form.Control size="lg" type="text" placeholder="Large text" />
-      </Tab>
-    </Tabs>
+        </Tab>
+      </Tabs>
+    </>
   );
 }
 
