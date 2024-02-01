fe31c39378d1ca515a877fb44db20870a53adc05
BotIzer <izerboti@gmail.com>
2024-02-01 11:56:57 +0100

    Popup component styled

--- src/components/FriendPopupActions.jsx
+++ src/components/FriendPopupActions.jsx
@@ -1,23 +1,22 @@
 import { Button } from "react-bootstrap";
 
 function FriendPopupActions() {
-    const actions = [
-        "Profile",
-        "Message",
-        "Remove Friend",
-    ]
+  const actions = ["Profile", "Message", "Remove Friend"];
 
-    const list = actions.map((action) => (
-        <Button className="list-group-item secondary h-100 p-2 custom-button" key={action}>
-            <div>{action}</div>
-        </Button>
-    ));
+  const list = actions.map((action) => (
+    <Button
+      className="list-group-item secondary h-100 w-100 p-2 custom-button m-0"
+      key={action}
+    >
+      <div>{action}</div>
+    </Button>
+  ));
 
-    return (
-            <div data-bs-theme="dark" className="d-grid w-25">
-                {list}
-            </div>
-    )
+  return (
+    <div data-bs-theme="dark" className="w-25 h-100 border">
+      {list}
+    </div>
+  );
 }
 
 export default FriendPopupActions;
