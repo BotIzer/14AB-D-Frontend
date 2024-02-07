99dc3ab3133be90679d893fa96a71b60c2b46f36
markneu22 <markneu22@gmail.com>
2024-02-07 12:05:44 +0100

    added getpagedetails

--- src/routes/UserPage.jsx
+++ src/routes/UserPage.jsx
@@ -5,10 +5,20 @@ import Col from "react-bootstrap/Col";
 import FriendPopupActions from "../components/FriendPopupActions";
 import { useState } from "react";
 import ChatWindow from "../components/ChatWindow";
-import {useParams} from "react-router-dom"
+import {useParams,useLocation} from "react-router-dom"
+import axios from "../api/axios";
 
 function UserPage() {
-  const {user} = useParams()
+  const location = useLocation();
+  const {user} = useParams(location.pathname.split('/')[2]);
+  const GetPageDetails = async ()  => { await axios.get(
+    `/user/${user}`,
+    {
+    },
+    {
+      headers: { 'Content-Type': 'application/json' }
+    }
+    )};
   const friends = ["Markneu22", "Lajtaib", "BotIzer", "Placeholder"];
   const [showPopup, setShowPopup] = useState(false);
   const [showChat, setShowChat] = useState(false);
