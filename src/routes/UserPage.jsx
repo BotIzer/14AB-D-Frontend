4e43d0d0f86c5e6dfb4a282219edbb8c0c6b555f
BotIzer <izerboti@gmail.com>
2024-02-07 12:05:20 +0100

    Userpage styling started

--- src/routes/UserPage.jsx
+++ src/routes/UserPage.jsx
@@ -6,47 +6,23 @@ import FriendPopupActions from "../components/FriendPopupActions";
 import { useState } from "react";
 import ChatWindow from "../components/ChatWindow";
 import {useParams} from "react-router-dom"
+import FriendList from "../components/FriendList";
 
 function UserPage() {
   const {user} = useParams()
   const friends = ["Markneu22", "Lajtaib", "BotIzer", "Placeholder"];
-  const [showPopup, setShowPopup] = useState(false);
-  const [showChat, setShowChat] = useState(false);
-  const list = friends.map((friend) => (
-    <Row key={friend}>
-      <Button
-        className=" secondary clear-button m-0"
-        to={"/user/" + friend}
-        onContextMenu={(e) => {
-          e.preventDefault();
-          setShowPopup(!showPopup);
-          setShowChat(false);
-        }}
-        onClick={(e) => {
-          e.preventDefault();
-          setShowChat(!showChat);
-          setShowPopup(false);
-        }}
-      >
-        {friend}
-      </Button>
-    </Row>
-  ));
+
+
   return (
     <>
       <Navigation></Navigation>
-      <Row className="w-100">
-        <Col
-          data-bs-theme="dark"
-          className="list-group list-group-flush p-2 h-100 overflow-auto text-center custom-border"
-        >
-          {list}
-        </Col>
-        <Col>
-          {showPopup ? <FriendPopupActions /> : null}
-          {showChat ? <ChatWindow /> : null}
-        </Col>
-      </Row>
+      <Container>
+        <Row className="w-100">
+          <Col className="border"><FriendList friends={friends}></FriendList></Col>
+          <Col className="border"></Col>
+          <Col className="border"></Col>
+        </Row>
+      </Container>
     </>
   );
 }
