477785d35c86d162b8a4aa7a244beabc99a1d0dd
markneu22 <markneu22@gmail.com>
2024-02-08 12:38:31 +0100

    Nevermind

--- src/routes/UserPage.jsx
+++ src/routes/UserPage.jsx
@@ -1,29 +1,44 @@
 import Navigation from "../components/Navigation";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { useParams, useLocation } from "react-router-dom";
+import {useParams, useLocation} from "react-router-dom";
 import { Container } from "react-bootstrap";
 import axios from "../api/axios";
 import FriendList from "../components/FriendList";
+import { useEffect, useState } from "react";
+import ErrorPage from "../error-page";
 
 export default function UserPage() {
+  
   const location = useLocation();
   const {user} = useParams(location.pathname.split('/')[2]);
-  const GetPageDetails = async ()  => { await axios.get(
-    `/user/${user}`,
-    {
-    },
-    {
-      headers: { 'Content-Type': 'application/json' }
-    }
-    )};
+  const [error, setError] = useState('');
 
+  useEffect(()=> {
+    const GetPageDetails = async ()  => { 
+      try { await axios.get(
+       `/user/${user}`,
+       {
+       },
+       {
+         headers: { 'Content-Type': 'application/json' }
+       },
+       )}
+     catch(err){
+       setError(err);
+     }};
+     GetPageDetails();
+  },[user]);
+  
+  if (error !== '') {
+    return <ErrorPage errorStatus={error}/>
+  }
   return (
     <>
       <Navigation></Navigation>
       <Container>
         <Row className="w-100">
-          <Col className="border"><FriendList friends={["Markneu22", "Lajtaib", "BotIzer", "Placeholder"]}>asd</FriendList></Col>
+          <Col className="border"><FriendList friends={["Markneu22", "Lajtaib", "BotIzer", "Placeholder"]}></FriendList></Col>
           <Col className="border"></Col>
           <Col className="border"></Col>
         </Row>
