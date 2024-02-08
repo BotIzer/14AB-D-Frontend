d4c855586ac7c5ee7cc0e5227e5fdfe599d17a28
markneu22 <markneu22@gmail.com>
2024-02-08 12:44:58 +0100

    asd

--- src/routes/UserPage.jsx
+++ src/routes/UserPage.jsx
@@ -13,6 +13,7 @@ export default function UserPage() {
   const location = useLocation();
   const {user} = useParams(location.pathname.split('/')[2]);
   const [error, setError] = useState('');
+asd
 
   useEffect(()=> {
     const GetPageDetails = async ()  => { 
