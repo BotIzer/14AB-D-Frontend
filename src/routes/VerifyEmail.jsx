b7a75d857185732e9e25d52f9e09ba60361052c2
markneu22 <markneu22@gmail.com>
2024-03-29 18:20:32 +0100

    updated .env and path

--- src/routes/VerifyEmail.jsx
+++ src/routes/VerifyEmail.jsx
@@ -4,12 +4,16 @@ import DesktopLayout from "../components/DesktopLayout";
 import MobileLayout from "../components/MoblieLayout";
 import { useEffect, useState } from "react";
 import axios from "../api/axios";
+import { useLocation } from "react-router-dom";
 
 export default function VerifyEmail() {
     const [response, setResponse] = useState(null);
+    const location = useLocation();
     useEffect(() => {
+        const path = `${import.meta.env.VITE_EMAIL_VERIFICATION}/${location.pathname.split('/')[2]}`
+        console.log(path)
         const VerifyEmail = async () => {
-           const response = await axios.get(import.meta.env.VITE_EMAIL_VERIFICATION, {
+           const response = await axios.get(path, {
                     headers: {
                       'Content-Type': 'application/json',
                       authorization: `Bearer null`,
