a415dc0a416b2c9ee456a08840f3d9dfe28c2ec8
markneu22 <markneu22@gmail.com>
2024-04-08 13:52:56 +0200

    re-added css styling

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,6 +2,7 @@ import Navigation from '../components/Navigation'
 import Container from 'react-bootstrap/Container'
 import DesktopLayout from '../components/DesktopLayout'
 import MobileLayout from '../components/MoblieLayout'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Home() {
   return (
