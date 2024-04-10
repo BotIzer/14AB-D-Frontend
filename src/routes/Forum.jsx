e2a0c7ff7571a2dda32c429d31ee892ac2520a03
markneu22 <markneu22@gmail.com>
2024-04-10 10:05:59 +0200

    added default image to forum page

--- src/routes/Forum.jsx
+++ src/routes/Forum.jsx
@@ -1,5 +1,5 @@
 import Navigation from '../components/Navigation'
-import { Col, Row, Container, Table, Button, Image, Pagination } from 'react-bootstrap'
+import { Col, Row, Container, Table, Button, Image as ReactImage, Pagination } from 'react-bootstrap'
 import PostCard from '../components/PostCard'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import axios from '../api/axios'
@@ -13,7 +13,7 @@ function Forum() {
     forumData: [],
     threads: []
   })
-
+  const [isBannerValid, setIsBannerValid] = useState(false)
 
   const categoryList = data.forumData[0] && data.forumData[0].tags.map((category)=>(
     <th
@@ -69,7 +69,18 @@ function Forum() {
     //  TODO: fix this ESLint error
   },[])
 
-
+  useEffect(()=>{
+    if(data.forumData.length !== 0){
+      const img = new Image();
+      img.src = data.forumData[0].banner
+      img.onload = ()=>{
+        setIsBannerValid(true)
+      }
+      img.onerror = ()=>{
+        setIsBannerValid(false)
+      }
+    }
+  },[data.forumData])
   return (
     <>
       <Navigation></Navigation>
@@ -88,7 +99,7 @@ function Forum() {
         <Row
           className='p-2'
           style={{
-            backgroundImage: data.forumData[0] && `url(${data.forumData[0].banner})`,
+            backgroundImage: data.forumData[0] && isBannerValid ? `url(${data.forumData[0].banner})` : `url(${import.meta.env.VITE_BFF_DEFAULT})`,
             backgroundSize: '100% 100%',
             backgroundRepeat: 'no-repeat',
             height: '20vh',
@@ -101,7 +112,7 @@ function Forum() {
           {localStorage.getItem('token') !== null ? <Button className='position-absolute end-0 rounded-pill clear-button' 
                 style={{width: 'auto', height: 'auto'}} 
                 onClick={()=>navigate(`/editforum/${encodeURIComponent(data.forumData[0].forum_name)}/${data.forumData[0]._id.forum_id}`)}>
-                  <Image src='/src/assets/icons/edit.png' className='hover-filter-gold'/>
+                  <ReactImage src='/src/assets/icons/edit.png' className='hover-filter-gold'/>
           </Button> : null}
         </Row>
         <Row className='no-padding-table'>
